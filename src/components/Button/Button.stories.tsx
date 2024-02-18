67497f48f4957883758c07cfa4e83f20fa3cb682
Jakub Szostko <jakub.szostko@cronn.de>
2024-02-18 11:51:51 +0100

    Fix issues in Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,19 +4,18 @@ import { Button } from './Button.tsx';
 const meta: Meta<typeof Button> = {
   title: 'Basics/Button',
   component: Button,
-  parameters: {
-    layout: 'centered',
-  },
 };
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
+// We specify component props inside 'args' field
+// This example is equivalent to <Button variant="contained">Label</Button>
 export const Contained: Story = {
   args: {
     variant: 'contained',
     children: 'Label',
-  }
+  },
 };
 
 export const Outlined: Story = {
