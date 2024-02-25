a97fdbb6a1fb0780f1cf0402fb43fac7d67dc671
Jakub Szostko <jakub.szostko@cronn.de>
2024-02-25 15:42:54 +0100

    Showcase action addon

---
+++ src/components/Slider/Slider.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Slider } from './Slider.tsx';
+
+const meta = {
+  title: 'Addons/Action Addon/Slider',
+  component: Slider,
+  // here you can define on which event you want to display log in the console
+  argTypes: {
+    onChange: { action: 'value changed' },
+  },
+} satisfies Meta<typeof Slider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    min: 0,
+    max: 20,
+    step: 1,
+    defaultValue: 6,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    min: 0,
+    max: 20,
+    step: 1,
+    defaultValue: 6,
+    disabled: true,
+  },
+};
---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,7 @@
+import { Slider as MuiSlider, SliderProps } from '@mui/material';
+
+export function Slider(props: SliderProps) {
+  return (
+    <MuiSlider {...props} />
+  );
+}
