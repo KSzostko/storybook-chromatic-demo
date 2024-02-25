c160c89a1455e8875a38fe53f6e2de2fd74526bc
Jakub Szostko <jakub.szostko@cronn.de>
2024-02-25 16:34:16 +0100

    Showcase controls addon

---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TextField } from './TextField.tsx';
+
+const meta = {
+  title: 'Addons/Controls Addon/TextField',
+  component: TextField,
+  // We can use controls addon to dynamically change prop values on each story
+  argTypes: {
+    variant: {
+      options: ['outlined', 'filled', 'standard'],
+      control: { type: 'radio' },
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    error: {
+      control: 'boolean',
+    },
+    helperText: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<typeof TextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'Name',
+    variant: 'standard',
+  },
+};
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,7 @@
+import { TextField as MuiTextField, TextFieldProps } from '@mui/material';
+
+export function TextField(props: TextFieldProps) {
+  return (
+    <MuiTextField {...props} />
+  );
+}
