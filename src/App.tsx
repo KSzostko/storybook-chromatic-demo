9cec216500d34949d1197ee925c408c29f4e16ab
Jakub Szostko <jakub.szostko@cronn.de>
2024-03-24 11:07:58 +0100

    Fix dependencies and lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PhotosList } from './components/PhotosLIst/PhotosList.tsx';
 import './App.css';
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const queryClient = new QueryClient();
 
 function App() {
